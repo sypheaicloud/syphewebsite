1ea2e0053667ffe6f2379b351325e74c7582fa36
JosiahBoziah <sypheit@gmail.com>
2026-02-02 19:59:08 -0600

    feat: add motion2.mp4 background to hero section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: Arsha
-* Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-* Updated: Feb 22 2025 with Bootstrap v5.3.3
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 
 (function() {
   "use strict";
